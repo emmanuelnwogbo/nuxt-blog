b7307ebea9bbf663b65f43b589320c8ad90bfe5f
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2020-08-02 02:17:31 +0100

    hgefwvgv

---
+++ pages/posts/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="posts-page">
+    <PostList :posts="loadedPosts" />
+  </div>
+</template>
+
+<script>
+import PostList from "@/components/Posts/PostList";
+
+export default {
+  components: {
+    PostList,
+  },
+  /*fetch(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: "1",
+              title: "First Post",
+              previewText: "This is our first post yo!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+            },
+            {
+              id: "2",
+              title: "Second Post",
+              previewText: "This is our second post!",
+              thumbnail:
+                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg",
+            },
+          ],
+        });
+      }, 1000);
+      // reject(new Error())
+    })
+      .then((data) => {
+        //return data;
+        context.store.commit("setPosts", data.loadedPosts);
+      })
+      .catch((e) => {
+        context.error(e);
+      });
+  },*/
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    },
+  },
+  /*created() {
+    this.$store.dispatch("setPosts", this.loadedPosts);
+  },*/
+};
+</script>
+
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
